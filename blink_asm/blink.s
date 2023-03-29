79426b7b9c8b9e6b2ce7fe2f081e95503e92b6b6
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2023-03-29 17:19:14 -0400

    updated blink_asm script to be more readable

--- blink_asm/blink.s
+++ blink_asm/blink.s
@@ -1,67 +1,57 @@
 .cpu cortex-m0
 .thumb
 
-init:
-	// write 32 to the reset clear register
-	ldr r0, rst_clr
-	mov r1, #32
-	str r1,[r0]
+.macro set_base a
+	ldr r3, =(\a)
+.endm
+
+// Writes a 32 bit value to the address at base + offset, where base is stored in r3
+.macro write value offset
+	ldr r0, =(\value)
+	mov r1, #\offset
+	str r0, [r3, r1] 
+.endm
+
+// Same as write, but for values which are small enough for a move immediate instruction,
+// slightly smaller and quicker
+.macro write_imm value offset
+	mov r0, #\value
+	mov r1, #\offset
+	str r0, [r3, r1] 
+.endm
+
+.section .text
+_stage2_boot:
+	// clear the IO_BANK0 bit of the reset register
+	set_base 0x4000f000
+	write_imm (1 << 5), 0
 
 // loop until rst_clr is equal to 0
-check_reset_cleared:
-	ldr r0, rst_base
-	ldr r1, rst_clr
-	cmp r1, #0
-	beq check_reset_cleared
+reset_clear_loop:
+	cmp r3, #0
+	beq reset_clear_loop
 
-	// Assign GPIO 17 to SIO control
-	ldr r0, gpio_17_ctrl_reg
-	mov r1, #5
-	str r1, [r0, #0]
+gpio_init:
+	// Configure GPIO 17 and 25 for control by the SIO subsystem (5)
+	set_base 0x40014000
+	write_imm 5, 0xCC
+	write_imm 5, 0x88
 
-	// set r1 to 1 << 17
-	// write 1 << 17 to the SIO Output Enable register
-	mov r1, #1
-	lsl r1, r1, #17
-	ldr r0, sio_base
-	str r1, [r0, #0x24]
+	// enable the leds
+	set_base 0xD0000000
+	write (1 << 17) | (1 << 25), 0x24
 
-// loop forever
 loop:
-	// toggle the LED by writing 1 << 17 to the GPIO_OUT_XOR register, then
-	// run the delay function
-	str r1, [r0, #0x1C]
-	ldr r3, delay_cycles
-	bl delay
+	// toggle the leds
+	write (1 << 17) | (1 << 25), 0x1C
 
+	ldr r4, =(0x000F0000)
+	bl delay
 	b loop
 
 // loads delay_cycles into a counter and subtracts 1 from it every cycle until
 // it reaches 0
 delay:
-	sub r3, #1
+	sub r4, #1
 	bne delay
 	bx lr
-
-// --------------------
-// CONSTANT DEFINITIONS
-// --------------------
-// A bank of constant values used by our program. Mostly these are register
-// addresses
-
-.align
-rst_base:
-	.word 0x4000c000
-
-rst_clr:
-	.word 0x4000f000
-
-gpio_17_ctrl_reg:
-	.word 0x4001408c 
-
-sio_base:
-	.word 0xd0000000
-
-// Number of cycles to delay for in the delay function
-delay_cycles:
-	.word 0x00400000
